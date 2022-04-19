37316293313343b1f1179ddf83b0e0ab529e457c
Joshua Lac <lacjosh@gmail.com>
2022-04-18 19:29:06 -0700

    V1 - BAREBONES

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1650335310,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/barframe-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/sinecontrol-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
