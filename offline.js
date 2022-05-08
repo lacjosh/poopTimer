6dbf78105e423d365345cdd20b878c03bcc03294
Joshua Lac <lacjosh@gmail.com>
2022-05-08 16:36:25 -0700

    Aesthetic Overhaul + Sounds

--- offline.js
+++ offline.js
@@ -1,18 +1,27 @@
 ﻿{
-	"version": 1650335619,
+	"version": 1652052676,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/barframe-sheet0.png",
+		"images/bar-sheet0.png",
 		"images/bg-sheet0.png",
 		"images/sinecontrol-sheet0.png",
+		"images/bg2-sheet0.png",
+		"media/stomachaudio.ogg",
+		"media/flushaudio.ogg",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"_handwritingfont-regular_1-webfont.eot",
+		"_handwritingfont-regular_1-webfont.svg",
+		"_handwritingfont-regular_1-webfont.ttf",
+		"_handwritingfont-regular_1-webfont.woff",
+		"stylesheet.css"
 	]
 }
